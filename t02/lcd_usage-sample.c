1b348ea8629b58d4189c3e7969f494dca1f11026
Jinxuan Cui <i@cjxol.com>
2018-02-14 17:31:04 +0000

    add task2 skeleton code

---
+++ t02/lcd_usage-sample.c
@@ -0,0 +1,27 @@
+/* COMP2215: Task 02---MODEL ANSWER */
+/* For La Fortuna board.            */
+
+
+#include <avr/io.h>
+#include "lcd.h"
+
+
+#define BUFFSIZE 256
+
+void init(void);
+
+void main(void) {
+    init();
+    display_string("Hello Southampton!\n");
+    display_string("Hello UK!\n");
+    display_string("Hello World!\n");
+}
+
+
+void init(void) {
+    /* 8MHz clock, no prescaling (DS, p. 48) */
+    CLKPR = (1 << CLKPCE);
+    CLKPR = 0;
+
+    init_lcd();
+}
